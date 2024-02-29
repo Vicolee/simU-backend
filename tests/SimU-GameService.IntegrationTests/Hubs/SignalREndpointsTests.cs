b11d0896c29f106eb5678260e0f0f50ee1e01527
Mark Lekina Rorat <marklekina@gmail.com>
2024-02-29 17:11:54 -0500

    intermediate fix to SendChat functionality

--- tests/SimU-GameService.IntegrationTests/Hubs/SignalREndpointsTests.cs
+++ tests/SimU-GameService.IntegrationTests/Hubs/SignalREndpointsTests.cs
@@ -18,11 +18,6 @@ public class SignalREndpointsTests : IClassFixture<TestWebApplicationFactory<Pro
         _client = _factory.CreateClient();
     }
 
-    private static HubConnection InitializeHubConnection(string authToken, string hubURL)
-        => new HubConnectionBuilder()
-            .WithUrl($"{hubURL}?access_token={authToken}")
-            .Build();
-
     private static async Task<HubConnection> StartConnectionAsync(
         HttpMessageHandler handler, string hubURL, string authToken)
     {
@@ -40,7 +35,7 @@ public class SignalREndpointsTests : IClassFixture<TestWebApplicationFactory<Pro
             
 
     [Fact]
-    public async void UpdateLocation_WhenUserExists_ShouldReturnOk()
+    public async void UpdateLocation_WhenUserExists_ShouldWork()
     {
         // arrange        
         // register user to get ID and auth token
@@ -55,7 +50,6 @@ public class SignalREndpointsTests : IClassFixture<TestWebApplicationFactory<Pro
         Random random = new();
         var location = new Location(random.Next(0, 100), random.Next(0, 100));
 
-        // act
         // initialize SignalR connection
         var hubURL = new Uri($"{Constants.Routes.BaseUri}{Constants.Routes.UnityHub.BaseUri}");
         _connection = await StartConnectionAsync(_factory.Server.CreateHandler(), hubURL!.ToString(), authToken);
@@ -68,20 +62,74 @@ public class SignalREndpointsTests : IClassFixture<TestWebApplicationFactory<Pro
         };
 
         // handle message received event
-        var tcs = new TaskCompletionSource<string>();
-        _connection.On<string, string>("MessageHandler", (sender, message) => tcs.SetResult(message));
-
+        var tcs = new TaskCompletionSource<(Guid, Location)>();
+        _connection.On<Guid, Location>("UpdateLocationHandler", (sender, location) => tcs.SetResult((sender, location)));
+        
+        // act
         // send location update to server
         await _connection.SendAsync("UpdateLocation", location);
-        string message = await tcs.Task;
+        var (user_id, user_location) = await tcs.Task;
         var userLocation = await TestUserUtils.GetUserLocation(_client, userId);
 
         // assert
-        Assert.NotNull(message);
-        Assert.Equal($"User {userId} has moved to ({location.X_coord}, {location.Y_coord})", message);
+        Assert.Equal(userId, user_id);
+        Assert.NotNull(userLocation);
+        Assert.Equal(location.X_coord, user_location!.X_coord);
+        Assert.Equal(location.Y_coord, user_location!.Y_coord);
 
         Assert.NotNull(userLocation);
         Assert.Equal(location.X_coord, userLocation!.X_coord);
         Assert.Equal(location.Y_coord, userLocation!.Y_coord);
+    }
+
+    [Fact]
+    public async Task SendChat_WhenSenderAndReceiverExist_ShouldWorkAsync()
+    {
+        // arrange
+
+        // register sender and receiver
+        var senderResponse = await TestUserUtils.RegisterUser(_client,
+            Constants.User.TestUsername,
+            Constants.User.TestEmail,
+            Constants.User.TestPassword);
+
+        var receiverResponse = await TestUserUtils.RegisterUser(_client,
+            Constants.User.TestUsername,
+            Constants.User.TestEmail,
+            Constants.User.TestPassword);
+
+        // initialize connection for sender and receiver        
+        var hubURL = new Uri($"{Constants.Routes.BaseUri}{Constants.Routes.UnityHub.Route}");
+        _connection = await StartConnectionAsync(
+            _factory.Server.CreateHandler(), hubURL!.ToString(), senderResponse!.AuthToken);
+        var receiverConnection = await StartConnectionAsync(
+            _factory.Server.CreateHandler(), hubURL!.ToString(), receiverResponse!.AuthToken);
+
+        // register handler for message received and closed connecting events
+        _connection.Closed += async error =>
+        {
+            await Task.Delay(new Random().Next(0, 5) * 1000);
+            await _connection.StartAsync();
+        };
+
+        receiverConnection.Closed += async error =>
+        {
+            await Task.Delay(new Random().Next(0, 5) * 1000);
+            await receiverConnection.StartAsync();
+        };
+        
+        var senderTcs = new TaskCompletionSource<(Guid, Location)>();
+        _connection.On<Guid, Location>("UpdateLocationHandler", (sender, location) => senderTcs.SetResult((sender, location)));
+
+        var receiverTcs = new TaskCompletionSource<(Guid, Location)>();
+        receiverConnection.On<Guid, Location>("UpdateLocationHandler", (sender, location) => receiverTcs.SetResult((sender, location)));
+        
+
+        // act
+        // send chat message from sender to receiver
+        
+
+        // assert
+        // check if receiver received the message
     }
 }
