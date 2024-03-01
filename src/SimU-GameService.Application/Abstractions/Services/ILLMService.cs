04457e283fe7bf6f3bacd0862d679b9700cc73e3
Alan Moss <alan.m.moss.jr.24@dartmouth.edu>
2024-03-01 00:30:03 -0500

    fixed conversation bugs and create agent bug

--- src/SimU-GameService.Application/Abstractions/Services/ILLMService.cs
+++ src/SimU-GameService.Application/Abstractions/Services/ILLMService.cs
@@ -5,7 +5,7 @@ public interface ILLMService
     Task<string> SendChat(Guid senderId, Guid recipientId, Guid conversationID, string content, bool streamResponse,
                           bool isSenderUser, bool isRecipientUser);
     Task<string> PromptForQuestion(Guid senderId, Guid recipientId, Guid conversationID, bool streamResponse, bool isRecipientUser);
-    Task EndConversation(Guid conversationID, IEnumerable<Guid> participants);
+    Task EndConversation(Guid conversationID, Guid participantA, Guid participantB);
     Task<string> GenerateCharacterSummary(Guid characterId, IEnumerable<string> questions,
                                           IEnumerable<IEnumerable<string>> answers);
     Task<Dictionary<string, string>?> GenerateAgentSprite(string description);
---
+++ src/SimU-GameService.Infrastructure/Migrations/20240229231308_paricipantIssue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimU_GameService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class paricipantIssue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Participants",
+                table: "Conversations");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ParticipantA",
+                table: "Conversations",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ParticipantB",
+                table: "Conversations",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParticipantA",
+                table: "Conversations");
+
+            migrationBuilder.DropColumn(
+                name: "ParticipantB",
+                table: "Conversations");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Participants",
+                table: "Conversations",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
